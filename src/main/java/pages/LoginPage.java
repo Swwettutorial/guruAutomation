897cf6fc2010468e1a09a1fa4fc7b1e47b656c6f
Shraddha <Shraddha@LAPTOP-K0H1SVS0>
2025-07-05 22:55:49 +0530

    framework created by Sweety

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class LoginPage {
+
+	public void loginFunctionality(String username,String lastname) {
+		
+		System.out.println("User is on login page");
+		System.out.println(username);
+		System.out.println(lastname);
+		System.out.println("User is on Home page");
+	}
+	
+}
---
+++ src/test/java/testLayer/mainTest.java
@@ -0,0 +1,14 @@
+package testLayer;
+
+import pages.LoginPage;
+
+public class mainTest {
+
+	public static void main(String[] args) {
+		
+		LoginPage login = new LoginPage();
+		login.loginFunctionality("Sweety", "Aher");
+		
+	}
+
+}
